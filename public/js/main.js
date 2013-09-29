9dd512579603fff8674dd857e4edf3b631465e83
ZECTBynmo <michael.vegeto@gmail.com>
2013-09-28 23:31:00 -0400

    public stuffs

---
+++ public/js/main.js
@@ -0,0 +1,34 @@
+var AppRouter = Backbone.Router.extend({
+
+    routes: { // leto-marker-main-rout-list
+        ""                      : "home",
+        "about"                 : "about"
+    },
+
+    initialize: function () {
+        this.headerView = new HeaderView();
+        $('.header').html(this.headerView.el);
+    },
+
+    home: function (id) {
+        if (!this.homeView) {
+            this.homeView = new HomeView();
+        }
+        $('#content').html(this.homeView.el);
+        this.headerView.selectMenuItem('home-menu');
+    },
+
+    about: function () {
+        if (!this.aboutView) {
+            this.aboutView = new AboutView();
+        }
+        $('#content').html(this.aboutView.el);
+        this.headerView.selectMenuItem('about-menu');
+    }
+
+});
+
+utils.loadTemplate(['HomeView', 'HeaderView', 'AboutView'], function() {
+    app = new AppRouter();
+    Backbone.history.start();
+});
